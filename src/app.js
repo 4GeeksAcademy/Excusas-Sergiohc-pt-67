38731a94e98b622d7d020053f65f0fcf84313409
Sergiohc19 <161878354+Sergiohc19@users.noreply.github.com>
2024-05-10 11:20:22 +0000

    Excusas-Terminadas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+// Lista de excusas de diferentes variables
+let who = ["El perro", "La abuela", "El coche", "Mi hermano"];
+let action = ["se comió", "se puso", "dejó", "perdió"];
+let what = ["mis deberes", "enferma", "de funcionar", "las llaves de casa"];
+let when = ["antes de salir", "anoche", "de camino", "ayer"];
+
+window.onload = function ExcuseGenerator() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const randomWho = who[Math.floor(Math.random() * who.length)];
+  const randomAction = action[Math.floor(Math.random() * action.length)];
+  const randomWhat = what[Math.floor(Math.random() * what.length)];
+  const randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  const excuse =
+    "¡¡¡" +
+    randomWho +
+    " " +
+    randomAction +
+    " " +
+    randomWhat +
+    " " +
+    randomWhen +
+    "!!!";
+
+  document.getElementById("excuse").innerHTML = excuse;
 };
